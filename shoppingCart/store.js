96820aa3521e0e989fe0666dc6dbc5ae4655f6f4
Lukas Mark Jakobsen <43439073+Draxlicious@users.noreply.github.com>
2020-12-23 12:10:14 +0100

    generate data and display + create formatCurrency from utils folder

---
+++ shoppingCart/store.js
@@ -0,0 +1,34 @@
+import items from "./items.json"
+import formatCurrency from "./util/formatCurrency"
+
+// console.log(items);
+
+const storeItemTemplate = document.querySelector("#store-item-template")
+const storeItemContainer = document.querySelector("[data-store-container]")
+const IMAGE_URL = "https://dummyimage.com/420x260/"
+
+export function setupStore(){
+    items.forEach(renderStoreItems)
+}
+
+function renderStoreItems(item){
+    const storeItem = storeItemTemplate.content.cloneNode(true)
+
+    const container = storeItem.querySelector("[data-store-item]")
+    container.dataset.itemId = item.id
+    
+    const name = storeItem.querySelector("[data-name]")
+    name.innerText = item.name
+
+    const category = storeItem.querySelector("[data-category]")
+    category.innerText = item.category
+
+    const image = storeItem.querySelector("[data-image]")
+    image.src = `${IMAGE_URL}/${item.imageColor}/${item.imageColor}`
+    
+    const price = storeItem.querySelector("[data-price]")
+    price.innerText = `${formatCurrency(item.priceCents / 100)}`
+
+    storeItemContainer.appendChild(storeItem)
+
+}
---
+++ shoppingCart/util/formatCurrency.js
@@ -0,0 +1,5 @@
+const formatter = new Intl.NumberFormat(undefined, { style: 'currency', currency: 'DKK'})
+
+export default function formatCurrency(amount){
+    return formatter.format(amount)
+}
